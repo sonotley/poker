34e0fa86a2df45e0a238adf3062ee6e857b097d3
Simon Notley <simonnotley@yahoo.co.uk>
2021-12-26 17:22:40 +0000

    Tidying up

--- main_prime.py
+++ main_prime.py
@@ -7,28 +7,19 @@ from math import prod
 import multiprocessing as mp
 import threading as tr
 
-parallelism = "THREAD"
-num_parallels = 8
+PARALLELISM = "THREAD"
+NUM_PARALLELS = 8
 
-if parallelism == "PROCESS":
+if PARALLELISM == "PROCESS":
     try:
-    	import psutil
+        import psutil
     except:
-    	psutil = None
+        psutil = None
     parallel = mp.Process
-elif parallelism == "THREAD":
+elif PARALLELISM == "THREAD":
     parallel = tr.Thread
 else:
     parallel = None
-
-
-# avoids using math.prod for backward compatibility, slows things up in cpython but allows pypy
-# from functools import reduce  # Required in Python 3
-# import operator
-#
-#
-# def prod(iterable):
-#     return reduce(operator.mul, iterable, 1)
 
 
 def find_best_score(board, hand, ranked_hands_dict):
@@ -47,7 +38,10 @@ def populate_boards(hand1, hand2, board=None):
         return tuple(set(x) for x in itertools.combinations(cards - deadcards, 5))
     elif len(board) < 5:
         deadcards = hand1 | hand2 | board
-        return tuple(set(x).union(board) for x in itertools.combinations(cards - deadcards, 5 - len(board)))
+        return tuple(
+            set(x).union(board)
+            for x in itertools.combinations(cards - deadcards, 5 - len(board))
+        )
     else:
         return (board,)
 
@@ -56,8 +50,8 @@ def get_board_and_score(ls, hand1, hand2, ranked_hands_dict, n, one, two, i):
     n_local = 0
     one_local = 0
     two_local = 0
-    print(len(ls),'--',i)
-    if parallelism == "PROCESS":      
+    print(len(ls), "--", i)
+    if PARALLELISM == "PROCESS":
         try:
             this_process = psutil.Process()
             this_process.cpu_affinity([i])
@@ -85,10 +79,10 @@ def get_board_and_score(ls, hand1, hand2, ranked_hands_dict, n, one, two, i):
 
     with n.get_lock():
         n.value += n_local
-    print(n_local, one_local, two_local,"--",i)
+    print(n_local, one_local, two_local, "--", i)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     print("Poker hand calculator, initialising...")
 
@@ -96,8 +90,8 @@ if __name__ == '__main__':
     hand1Wins = mp.Value(typecode_or_type=c_longlong)
     hand2Wins = mp.Value(typecode_or_type=c_longlong)
 
-    with open('hands_prime.json','r') as f:
-        ranked_hands_dict={int(k):v for k,v in json.load(f).items()}
+    with open("hands_prime.json", "r") as f:
+        ranked_hands_dict = {int(k): v for k, v in json.load(f).items()}
 
     hands = []
     board = None
@@ -113,7 +107,7 @@ if __name__ == '__main__':
                 except KeyError:
                     print("Not a valid input")
 
-    if input('Do you know the flop? ').lower() in ['y', 'yes']:
+    if input("Do you know the flop? ").lower() in ["y", "yes"]:
         flop = []
         for card in range(3):
             valid_input = False
@@ -130,15 +124,37 @@ if __name__ == '__main__':
     hand2_primes = set(hands[2:])
 
     tic = time.time()
-    possible_boards = populate_boards(hand1_primes, hand2_primes,board)
+    possible_boards = populate_boards(hand1_primes, hand2_primes, board)
 
     parallels = []
-    if num_parallels == 0 or parallel is None:
-        get_board_and_score(possible_boards,hand1_primes,hand2_primes,ranked_hands_dict, draws, hand1Wins, hand2Wins, 0)
+    if NUM_PARALLELS == 0 or parallel is None:
+        get_board_and_score(
+            possible_boards,
+            hand1_primes,
+            hand2_primes,
+            ranked_hands_dict,
+            draws,
+            hand1Wins,
+            hand2Wins,
+            0,
+        )
     else:
-        for i in range(num_parallels):
-            parallels.append(parallel(target=get_board_and_score,
-                                        args=(possible_boards[i::num_parallels], hand1_primes, hand2_primes, ranked_hands_dict, draws, hand1Wins, hand2Wins, i)))
+        for i in range(NUM_PARALLELS):
+            parallels.append(
+                parallel(
+                    target=get_board_and_score,
+                    args=(
+                        possible_boards[i::NUM_PARALLELS],
+                        hand1_primes,
+                        hand2_primes,
+                        ranked_hands_dict,
+                        draws,
+                        hand1Wins,
+                        hand2Wins,
+                        i,
+                    ),
+                )
+            )
             parallels[i].start()
 
         for p in parallels:
@@ -147,6 +163,11 @@ if __name__ == '__main__':
         print("Joined")
     print(draws.value, hand1Wins.value, hand2Wins.value)
     total = draws.value + hand1Wins.value + hand2Wins.value
-    print(f'Draw: {draws.value/total}', f'Hand 1: {hand1Wins.value / total}', f'Hand 2: {hand2Wins.value/total}', sep='\n')
+    print(
+        f"Draw: {draws.value/total}",
+        f"Hand 1: {hand1Wins.value / total}",
+        f"Hand 2: {hand2Wins.value/total}",
+        sep="\n",
+    )
     toc = time.time()
     print(toc - tic)
