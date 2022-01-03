55dd59b78d1b83a8f1d286b1ec80404e1e49a943
Simon Notley <simonnotley@yahoo.co.uk>
2022-01-03 20:36:43 +0000

    Added new interactive mode and refactored for tidier code

--- main_prime.py
+++ main_prime.py
@@ -10,24 +10,11 @@ import multiprocessing as mp
 from multiprocessing.sharedctypes import Synchronized
 import threading as tr
 import argparse
-from os import environ
 
-env_parallelism = environ.get("POKER_PARALLELISM")
-env_num = environ.get("POKER_NUM_PARA")
-
-PARALLELISM = "PROCESS" if env_parallelism is None else env_parallelism
-NUM_PARALLELS = 0 if env_num is None else int(env_num)
-
-if PARALLELISM == "PROCESS":
-    try:
-        import psutil
-    except ImportError:
-        psutil = None
-    parallel = mp.Process
-elif PARALLELISM == "THREAD":
-    parallel = tr.Thread
-else:
-    parallel = None
+try:
+    import psutil
+except ImportError:
+    psutil = None
 
 
 def find_best_score(
@@ -67,8 +54,13 @@ def get_possible_hands_and_boards(
     possible_boards = get_possible_boards(hand1, hand2, board)
     for possible_board in possible_boards:
         deadcards = hand1 | hand2 | possible_board
-        possible_hands = [set(x).union(hand2) for x in itertools.combinations(cards - deadcards, 2 - len(hand2))]
-        possible_combinations.extend(zip(possible_hands, (possible_board,)*len(possible_hands)))
+        possible_hands = [
+            set(x).union(hand2)
+            for x in itertools.combinations(cards - deadcards, 2 - len(hand2))
+        ]
+        possible_combinations.extend(
+            zip(possible_hands, (possible_board,) * len(possible_hands))
+        )
     return tuple(possible_combinations)
 
 
@@ -87,13 +79,12 @@ def get_board_and_score(
     one_local = 0
     two_local = 0
     print(len(possible_hands_and_boards), "--", parallelism_index)
-    if PARALLELISM == "PROCESS":
-        try:
-            this_process = psutil.Process()
-            this_process.cpu_affinity([parallelism_index])
 
-        except AttributeError:
-            print("CPU affinity not supported")
+    try:
+        this_process = psutil.Process()
+        this_process.cpu_affinity([parallelism_index])
+    except AttributeError:
+        print("CPU affinity not supported")
 
     for hand2, board in possible_hands_and_boards:
 
@@ -118,57 +109,20 @@ def get_board_and_score(
     print(draw_local, one_local, two_local, "--", parallelism_index)
 
 
-if __name__ == "__main__":
-
-    print("Poker hand calculator, initialising...")
-
+def calculate_probabilities(
+    hand1: set[int],
+    hand2: set[int] | None,
+    board: set[int] | None,
+    num_parallels: int = 0,
+    process_parallelism: bool = True,
+):
     draws = mp.Value(typecode_or_type=c_longlong)
     hand1Wins = mp.Value(typecode_or_type=c_longlong)
     hand2Wins = mp.Value(typecode_or_type=c_longlong)
 
-    with open("hands_prime.json", "r") as f:
-        ranked_hands_dict = {int(k): v for k, v in json.load(f).items()}
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "--hand1", "-h1", help="Cards in hand one as a string", type=str
-    )
-    parser.add_argument(
-        "--hand2", "-h2", help="Cards in hand one as a string", type=str, default=None
-    )
-    parser.add_argument(
-        "--board",
-        "-b",
-        help="Cards in the flop and optionally turn and river",
-        type=str,
-        default=None,
-    )
-
-    args = parser.parse_args()
-
-    hand1 = (
-        cardsutils.extract_cards_from_string(args.hand1)
-        if args.hand1 is not None
-        else set()
-    )
-    hand2 = (
-        cardsutils.extract_cards_from_string(args.hand2)
-        if args.hand2 is not None
-        else set()
-    )
-    board = (
-        cardsutils.extract_cards_from_string(args.board)
-        if args.board is not None
-        else set()
-    )
-
-    tic = time.time()
-    # possible_boards = get_possible_boards(hand1, hand2, board)
     possible_hands_and_boards = get_possible_hands_and_boards(hand1, hand2, board)
 
-    parallels = []
-    if NUM_PARALLELS == 0 or parallel is None:
+    if num_parallels == 0:
         get_board_and_score(
             hand1,
             possible_hands_and_boards,
@@ -179,13 +133,15 @@ if __name__ == "__main__":
             0,
         )
     else:
-        for i in range(NUM_PARALLELS):
+        parallels = []
+        parallel = mp.Process if process_parallelism else tr.Thread
+        for i in range(num_parallels):
             parallels.append(
                 parallel(
                     target=get_board_and_score,
                     args=(
                         hand1,
-                        possible_hands_and_boards[i::NUM_PARALLELS],
+                        possible_hands_and_boards[i::num_parallels],
                         ranked_hands_dict,
                         draws,
                         hand1Wins,
@@ -200,13 +156,86 @@ if __name__ == "__main__":
             p.join()
 
         print("Joined")
-    print(draws.value, hand1Wins.value, hand2Wins.value)
-    total = draws.value + hand1Wins.value + hand2Wins.value
-    print(
-        f"Draw: {draws.value/total}",
-        f"Hand 1: {hand1Wins.value / total}",
-        f"Hand 2: {hand2Wins.value/total}",
-        sep="\n",
+
+    return draws.value, hand1Wins.value, hand2Wins.value
+
+
+def load_ranked_hands_dict(filepath: str = "hands_prime.json"):
+    with open(filepath, "r") as f:
+        return {int(k): v for k, v in json.load(f).items()}
+
+
+def get_arg_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--hand1", "-h1", help="Cards in hand one as a string", type=str, default=None
     )
-    toc = time.time()
-    print(toc - tic)
+    parser.add_argument(
+        "--hand2", "-h2", help="Cards in hand one as a string", type=str, default=set()
+    )
+    parser.add_argument(
+        "--board",
+        "-b",
+        help="Cards in the flop and optionally turn and river",
+        type=str,
+        default=set(),
+    )
+    parser.add_argument(
+        "--num-parallels",
+        "-n",
+        help="Number of parallel processing entities (0 means no parallelism)",
+        type=int,
+        default=0,
+    )
+
+    parser.add_argument(
+        "--thread-parallelism",
+        "-t",
+        help="Use thread rather than process parallelism",
+        type=bool,
+        default=False,
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+
+    print("Poker hand calculator, initialising...")
+    ranked_hands_dict = load_ranked_hands_dict()
+    print("Initialisation complete")
+
+    parser = get_arg_parser()
+    args = parser.parse_args()
+
+    first_run = True
+    interactive = True if not args.hand1 else False
+
+    while interactive or first_run:
+        if interactive:
+            raw_args = input("Running in interactive mode, please enter your arguments: ")
+            args = parser.parse_args(raw_args.split())
+
+        hand1, hand2, board = [cardsutils.extract_cards_from_string(x) for x in (args.hand1, args.hand2, args.board)]
+
+        tic = time.time()
+        draws, hand1_wins, hand2_wins = calculate_probabilities(
+            hand1,
+            hand2,
+            board,
+            num_parallels=args.num_parallels,
+            process_parallelism=(not args.thread_parallelism),
+        )
+        toc = time.time()
+        print(f"Calculation complete in {toc - tic:.1f} seconds")
+
+        print(draws, hand1_wins, hand2_wins)
+        total = draws + hand1_wins + hand2_wins
+        print(
+            f"Draw: {draws / total}",
+            f"Hand 1: {hand1_wins / total}",
+            f"Hand 2: {hand2_wins / total}",
+            sep="\n",
+        )
+        first_run = False
--- readme.md
+++ readme.md
@@ -6,23 +6,26 @@ It is not optimised for useability or nice code, but it is pretty optimised for 
 I use it as a toy project to implement in new languages I am learning and to test the speed of different
 machines, OSes., Python versions, etc.
 
-Given two hands in Texas Hold'Em (only two players currently supported, it would be insanely slow with more) 
-and optionally the community cards it calculates the win probability for each hand by calculating every possible outcome.
+Given one or two hands in Texas Hold'Em (only two players currently supported) 
+and some, none or all of the community cards it calculates the win probability for each hand by 
+calculating every possible outcome.
 
 ## How to use
 
 First run `build_json.py` to create a json file that contains the value of every possible hand. You only need to do this once.
 This has a dependency on the `primesieve` package.
 
-Now run `main_prime.py`. See below for arguments. 
+Now run `main_prime.py`. See below for arguments or use `--help`. 
 
-    main_primes.py --hand1 2h3h [--hand2 KdAs] [--board 5s6s7s]
+    main_primes.py --hand1 2h3h [--hand2 KdAs] [--board 5s6s7s] [--num-parallels 8] [--thread-parallelism]
 
 Technically you can omit both `hand2` and `board` but you'll be waiting a very long time for an answer. 
-You can also supply partial definitions of either optional parameter, i.e. 0-2 cards for `hand2` and 0-5 for `board`.
+You can also supply partial definitions of either parameter, i.e. 0-2 cards for `hand2` and 0-5 for `board`.
 
-You can control the parallelism by setting the environment variables `POKER_PARALLELISM` and `POKER_NUM_PARA`.
+Running without any parameters starts in interactive mode where you will be prompted to enter arguments.
 
-In standard CPython on Linux it is usually fastest to select `PARALLELISM = "PROCESS"` and set `NUM_PARALLELS`
+In standard CPython on Linux it is usually fastest to set `num-parallels`
 equal to the number of cores you have. However, on other platforms (or very high core-counts) the overhead to start a 
-process is so high that a lower value is better. `NUM_PARALLELS = 0` disables parallelism entirely.
+process is so high that a lower value is better. `num-parallels = 0` disables parallelism entirely. 
+The `--thread-parallelism` switch changes the programme to use threads instead of processes - 
+this is useless unless you are using a GIL-free interpreter
